40b8c2cca99553b0cd0cebb74069c4b4f8393921
JT <certainthings@gmail.com>
2020-12-10 12:24:52 +1100

    loops bonus

--- 01-javascript/loops/js/exercises.js
+++ 01-javascript/loops/js/exercises.js
@@ -18,7 +18,9 @@ for (let n = 0; n <= 20; n++) {
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
 
 for (let i = 0; i <= 10; i++) {
-  console.log(`${ i } * 9 = ${ i * 9 }`);
+  for (let j = 0; j <= 10; j++) {
+    console.log(`${ i } * ${ j } = ${ i * j }`);
+  }
 }
 
 // Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10.
