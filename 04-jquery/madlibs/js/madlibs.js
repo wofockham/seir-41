a085b04f927588d463fedb3fcaa41a132576e0d0
JT <certainthings@gmail.com>
2020-12-18 15:43:22 +1100

    fun text

--- 04-jquery/madlibs/js/madlibs.js
+++ 04-jquery/madlibs/js/madlibs.js
@@ -1,11 +1,16 @@
-const makeMadLib = function () {
-  const noun = $('#noun').val();
-  const adjective = $('#adjective').val();
-  const name = $('#person').val();
+$(document).ready(function () {
+  const makeMadLib = function () {
+    const noun = $('#noun').val();
+    const adjective = $('#adjective').val();
+    const name = $('#person').val();
 
-  const story = `${ name } really likes ${ adjective } ${ noun }.`;
+    const story = `${ name } really likes ${ adjective } ${ noun }.`;
 
-  $('#story').text(story);
-};
+    $('#story').text(story);
+  };
 
-$('#lib-button').on('click', makeMadLib);
+  $('#lib-button').on('click', makeMadLib);
+
+  $('h1').funText(100, 'candy');
+  $('li').funText(50, 'reverseRainbow');
+});
