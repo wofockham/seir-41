50801d0f1c46694eb24302154b88debb8e9b1ddc
JT <certainthings@gmail.com>
2021-01-14 14:36:50 +1100

    butterflies index

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -2,7 +2,16 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
-# at least one route
 get '/' do
   erb :home
+end
+
+# INDEX
+get '/butterflies' do
+  # connect to the db
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  @butterflies = db.execute 'SELECT * FROM butterflies'
+  db.close
+  erb :butterflies_index
 end
