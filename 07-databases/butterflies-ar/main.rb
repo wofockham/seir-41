6584c120e05317987badc39397b7b78bc6a4bb09
JT <certainthings@gmail.com>
2021-01-15 15:26:10 +1100

    plants CRUD

---
+++ 07-databases/butterflies-ar/main.rb
@@ -0,0 +1,122 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+require 'active_record'
+
+# Rails will do this for you automatically:
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'database.sqlite3'
+)
+
+# Optional bonus
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+class Butterfly < ActiveRecord::Base
+end
+
+class Plant < ActiveRecord::Base
+end
+
+get '/' do
+  erb :home
+end
+
+# INDEX
+get '/butterflies' do
+  @butterflies = Butterfly.all
+  erb :butterflies_index
+end
+
+# NEW
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+# CREATE
+post '/butterflies' do
+  butterfly = Butterfly.new
+  butterfly.name = params[:name]
+  butterfly.family = params[:family]
+  butterfly.image = params[:image]
+  butterfly.save
+  redirect to("/butterflies/#{ butterfly.id }") # GET show
+end
+
+# SHOW
+get '/butterflies/:id' do
+  @butterfly = Butterfly.find params[:id]
+  erb :butterflies_show
+end
+
+# EDIT
+get '/butterflies/:id/edit' do
+  @butterfly = Butterfly.find params[:id]
+  erb :butterflies_edit
+end
+
+# UPDATE
+post '/butterflies/:id' do
+  butterfly = Butterfly.find params[:id]
+  butterfly.name = params[:name]
+  butterfly.family = params[:family]
+  butterfly.image = params[:image]
+  butterfly.save
+  redirect to("/butterflies/#{ params[:id] }") # GET
+end
+
+# DESTROY
+get '/butterflies/:id/delete' do
+  butterfly = Butterfly.find params[:id]
+  butterfly.destroy
+  redirect to('/butterflies')
+end
+
+# PLANTS #######################################################################
+# INDEX
+get '/plants' do
+  @plants = Plant.all
+  erb :plants_index
+end
+
+# NEW
+get '/plants/new' do
+  erb :plants_new
+end
+
+# CREATE
+post '/plants' do
+  plant = Plant.new
+  plant.name = params[:name]
+  plant.image = params[:image]
+  plant.save
+  redirect to("/plants/#{ plant.id }") # GET show
+end
+
+# SHOW
+get '/plants/:id' do
+  @plant = Plant.find params[:id]
+  erb :plants_show
+end
+
+# EDIT
+get '/plants/:id/edit' do
+  @plant = Plant.find params[:id]
+  erb :plants_edit
+end
+
+# UPDATE
+post '/plants/:id' do
+  plant = Plant.find params[:id]
+  plant.name = params[:name]
+  plant.image = params[:image]
+  plant.save
+  redirect to("/plants/#{ plant.id }") # GET show
+end
+
+# DESTROY
+get '/plants/:id/delete' do
+  plant = Plant.find params[:id]
+  plant.destroy
+  redirect to('/plants')
+end
