15c3d0279d13db39783fb683c27379bcb0171dae
JT <certainthings@gmail.com>
2021-01-21 17:26:53 +1100

    user sign in and logout for trent

--- 08-rails/tunr/config/routes.rb
+++ 08-rails/tunr/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
   resources :users, :only => [:new, :create]
+
+  get '/login' => 'session#new'        # login form
+  post '/login' => 'session#create'    # process the login
+  delete '/login' => 'session#destroy' # log out
 end
