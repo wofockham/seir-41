6e515d0cbef8ed6a24dc55b5881cd1f5ff62f607
JT <certainthings@gmail.com>
2021-01-21 15:34:14 +1100

    user sign up

--- 08-rails/tunr/db/schema.rb
+++ 08-rails/tunr/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_013855) do
+ActiveRecord::Schema.define(version: 2021_01_21_035046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_01_21_013855) do
     t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
