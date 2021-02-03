ef04a2944d9ac6ddd1d4f911f5118a022769bb74
JT <certainthings@gmail.com>
2021-02-03 17:03:49 +1100

    AJAX image search

---
+++ 10-ajax/image-search/js/search.js
@@ -0,0 +1,52 @@
+const searchFlickr = function (keywords) {
+  console.log('Searching for', keywords);
+  const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?'; // JSONP
+  $.getJSON(flickrURL, {
+    method: 'flickr.photos.search', // not to be confused with HTTP methods like GET/POST
+    api_key: '2f5ac274ecfac5a455f38745704ad084',
+    text: keywords,
+    format: 'json'
+  }).done(showImages).done(function (info) {
+    console.log(info);
+  });
+};
+
+const showImages = function (results) {
+  _(results.photos.photo).each(function (photo) {
+    const thumbnailURL = generateURL(photo);
+    const $img = $('<img>', {src: thumbnailURL}); // .attr('src', thumbnailURL);
+    $img.appendTo('#images'); // $('#images').append($img);
+  });
+};
+
+const generateURL = function (p) {
+  return [
+    'http://farm',
+    p.farm,
+    '.static.flickr.com/',
+    p.server,
+    '/',
+    p.id,
+    '_',
+    p.secret,
+    '_q.jpg' // Change 'q' to something else for different sizes (see docs)
+  ].join('');
+};
+
+$(document).ready(function () {
+  $('#search').on('submit', function (event) {
+    event.preventDefault(); // disable the form being submitted to "the server".
+
+    const searchTerms = $('#query').val();
+    searchFlickr(searchTerms);
+  });
+
+  // Very twitchy:
+  $(window).on('scroll', function () {
+    const scrollBottom = $(document).height() - $(document).scrollTop() - $(window).height();
+    if (scrollBottom < 500) {
+      const searchTerms = $('#query').val();
+      searchFlickr(searchTerms); // getting page 1 again.
+    }
+  });
+});
