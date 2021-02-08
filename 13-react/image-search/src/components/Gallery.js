91aaa59d80f4fc7e27491d9e00b05b6fa84e5b40
JT <certainthings@gmail.com>
2021-02-08 16:06:11 +1100

    that was quite the day

--- 13-react/image-search/src/components/Gallery.js
+++ 13-react/image-search/src/components/Gallery.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-class Gallery extends Component {
-  render() {
-    return (
-      <h2>Gallery coming soon</h2>
-    );
-  }
-}
+// Functional components: for when you don't need state.
+// this.props becomes simply props (once you "accept" it in the function)
+const Gallery = (props) => {
+  return (
+    <div>
+      { props.images.map( (url) => <img key={ url } src={ url } /> ) }
+    </div>
+  );
+};
 
 export default Gallery;
