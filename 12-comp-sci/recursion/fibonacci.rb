4b8a88009d85bcca4c209e0ef0b3c8b5ced9dd06
JT <certainthings@gmail.com>
2021-02-08 11:51:30 +1100

    faster recursion

--- 12-comp-sci/recursion/fibonacci.rb
+++ 12-comp-sci/recursion/fibonacci.rb
@@ -23,6 +23,26 @@ end
 # Bonus homework:
 # Make this faster but stil recursive:
 # Option a: memoisation
-# Option b: iterative recursion
+
+def fib_memo(n)
+  @fib ||= {} # equivalent @fib = @fib || {}
+
+  if @fib[n]
+    @fib[n] # return the value we saved earlier.
+  elsif n == 1 || n == 2
+    1
+  else
+    @fib[n] = fib_memo(n-1) + fib_memo(n-2) # memoisation
+  end
+end
+
+# Option b: faster, iterative recursion
+def fib(n, a=1, b=1)
+  if n == 1 || n == 2
+    b
+  else
+    fib n-1, b, a+b # mystical
+  end
+end
 
 binding.pry
