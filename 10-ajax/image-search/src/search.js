1de075b030f3acecccff94e18704d6a9553b5e98
JT <certainthings@gmail.com>
2021-02-24 17:08:58 +1100

    transpiling with the miles davis quintet

--- 10-ajax/image-search/src/search.js
+++ 10-ajax/image-search/src/search.js
@@ -1,25 +1,23 @@
-'use strict';
-
-var state = {
+const state = {
   nextPage: 1,
   lastPageReached: false
 };
 
-var searchFlickr = function searchFlickr(keywords) {
+const searchFlickr = function (keywords) {
   if (state.lastPageReached) {
     return; // Get out.
   }
 
-  console.log('Searching Flickr for ' + keywords + '!!!');
+  console.log(`Searching Flickr for ${ keywords }!!!`);
 
-  var flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?'; // JSONP
+  const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?'; // JSONP
 
   $.getJSON(flickrURL, {
     method: 'flickr.photos.search', // not to be confused with HTTP methods like GET/POST
     api_key: '2f5ac274ecfac5a455f38745704ad084',
     text: keywords,
     format: 'json',
-    page: state.nextPage++
+    page: state.nextPage++,
   }).done(showImages).done(function (info) {
     if (info.photos.page >= info.photos.pages) {
       state.lastPageReached = true;
@@ -29,16 +27,25 @@ var searchFlickr = function searchFlickr(keywords) {
   });
 };
 
-var showImages = function showImages(results) {
+const showImages = function (results) {
   _(results.photos.photo).each(function (photo) {
-    var thumbnailURL = generateURL(photo);
-    var $img = $('<img>', { src: thumbnailURL }); // .attr('src', thumbnailURL);
+    const thumbnailURL = generateURL(photo);
+    const $img = $('<img>', {src: thumbnailURL}); // .attr('src', thumbnailURL);
     $img.appendTo('#images'); // $('#images').append($img);
   });
 };
 
-var generateURL = function generateURL(p) {
-  return ['http://farm', p.farm, '.static.flickr.com/', p.server, '/', p.id, '_', p.secret, '_q.jpg' // Change 'q' to something else for different sizes (see docs)
+const generateURL = function (p) {
+  return [
+    'http://farm',
+    p.farm,
+    '.static.flickr.com/',
+    p.server,
+    '/',
+    p.id,
+    '_',
+    p.secret,
+    '_q.jpg' // Change 'q' to something else for different sizes (see docs)
   ].join('');
 };
 
@@ -51,17 +58,17 @@ $(document).ready(function () {
     state.lastPageReached = false;
     $('#images').empty();
 
-    var searchTerms = $('#query').val();
+    const searchTerms = $('#query').val();
     searchFlickr(searchTerms);
   });
 
-  var chillSearchFlickr = _.debounce(searchFlickr, 4000, true);
+  const chillSearchFlickr = _.debounce( searchFlickr, 4000, true );
 
   // Very twitchy:
   $(window).on('scroll', function () {
-    var scrollBottom = $(document).height() - $(document).scrollTop() - $(window).height();
+    const scrollBottom = $(document).height() - $(document).scrollTop() - $(window).height();
     if (scrollBottom < 500) {
-      var searchTerms = $('#query').val();
+      const searchTerms = $('#query').val();
       chillSearchFlickr(searchTerms); // getting page 1 again.
     }
   });
