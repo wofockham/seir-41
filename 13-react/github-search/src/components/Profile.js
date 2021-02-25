ae5e673b9956c7cabb3756f5d12a49bf2da067c7
JT <certainthings@gmail.com>
2021-02-25 15:11:26 +1100

    github search with conditional rendering

--- 13-react/github-search/src/components/Profile.js
+++ 13-react/github-search/src/components/Profile.js
@@ -13,18 +13,68 @@ class Profile extends Component {
   // React lifecycle method
   componentDidMount() {
     const { username } = this.props.match.params;
+
     Github.getUserInfo(username).then((result) => {
       this.setState({user: result.data});
     });
 
-    Github.getUserRepos(username);
+    Github.getUserRepos(username).then((result) => {
+      this.setState({repos: result.data});
+    });
   }
 
   render() {
     return (
-      <h2>{ this.props.match.params.username } coming soon</h2>
+      <div>
+        <h2>Profile for { this.props.match.params.username }</h2>
+        <UserInfo user={ this.state.user } />
+        <Repositories repos={  this.state.repos } />
+      </div>
     );
   }
+}
+
+const UserInfo = (props) => {
+  // Conditional rendering
+  if (props.user === null) {
+    return (<div>Loading...</div>);
+  }
+
+  const { login, bio, followers, following, public_repos, public_gists } = props.user;
+
+  return (
+    <div>
+      <h3>Stats for { login }</h3>
+      <p>Bio: { bio }</p>
+      <p>Followers: { followers }</p>
+      <p>Following: { following }</p>
+      <p>Public repos: { public_repos }</p>
+      <p>Public gists: { public_gists }</p>
+    </div>
+  );
+};
+
+const Repositories = (props) => {
+  if (props.repos === null) {
+    return (<div>Loading...</div>);
+  }
+
+  const userRepos = props.repos.map((r) => (
+    <li key={ r.id }>
+      <a href={ r.html_url } target="_blank">
+        { r.name }
+      </a>
+    </li>
+  ));
+
+  return (
+    <div>
+      <h3>User Repos</h3>
+      <ul>
+        { userRepos }
+      </ul>
+    </div>
+  );
 }
 
 export default Profile;
--- 13-react/github-search/src/github.js
+++ 13-react/github-search/src/github.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const Github = {
-  getUserInfo: function (username) {
+  getUserInfo(username) {
     // Return the Promise so that <Profile /> can attach the .then it needs.
     return axios.get(`https://api.github.com/users/${ username }?client_id=4ec1b8f9c149f7c46ffd&client_secret=a05ec287c29ebd9633131b5a08298530709130e9`);
   },
 
-  getUserRepos: function (username) {
-    console.log('getting user repos for', username);
+  getUserRepos(username) {
+    console.log('getting user repos');
+    return axios.get(`https://api.github.com/users/${ username }/repos?client_id=4ec1b8f9c149f7c46ffd&client_secret=a05ec287c29ebd9633131b5a08298530709130e9`);
   }
 };
 
