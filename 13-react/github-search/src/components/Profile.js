9f603327898c4e7a4708307b7bdae3bf07a7a094
JT <certainthings@gmail.com>
2021-02-25 12:45:42 +1100

    github search is almost working

--- 13-react/github-search/src/components/Profile.js
+++ 13-react/github-search/src/components/Profile.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Github from '../github';
 
-const Profile = () => {
-  return (
-    <h2>Profile coming soon</h2>
-  );
-};
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      repos: null
+    }
+  }
+
+  // React lifecycle method
+  componentDidMount() {
+    const { username } = this.props.match.params;
+    Github.getUserInfo(username).then((result) => {
+      this.setState({user: result.data});
+    });
+
+    Github.getUserRepos(username);
+  }
+
+  render() {
+    return (
+      <h2>{ this.props.match.params.username } coming soon</h2>
+    );
+  }
+}
 
 export default Profile;
