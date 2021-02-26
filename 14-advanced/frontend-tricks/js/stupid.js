39d61955c701383f8d207ed4c17742bc72d4ec66
JT <certainthings@gmail.com>
2021-02-26 17:10:10 +1100

    stupid frontend tricks

---
+++ 14-advanced/frontend-tricks/js/stupid.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  // Parallax //////////////////////////////////////////////////////////////////
+  const $kitty = $('.kitty-js'); // Cacheing.
+  const $body = $('body');
+
+  $(window).on('scroll', () => {
+    const scrollTop = $(window).scrollTop();
+    $kitty.css('background-position-y', -scrollTop / 2);
+    $body.css('background-position-y', -scrollTop / 5);
+  });
+
+  // Bubbles ///////////////////////////////////////////////////////////////////
+  $(window).on('mousemove', (e) => {
+    const {pageX:x, pageY:y} = e; // Destructuring with renaming.
+
+    const size = Math.random() * 10;
+
+    const $bubble = $('<div class="bubble">').css({
+      left: x,
+      top: y,
+      width: size + 'em',
+      height: size + 'em'
+    });
+
+    $bubble.appendTo('body');
+
+    $bubble.animate({top: -200}, 2500, () => {
+      $bubble.remove(); // this callback runs when the animation is complete.
+    });
+
+  })
+});
