7c37429d7389a886e09ed7d47c355dd19ca04faf
JT <certainthings@gmail.com>
2021-03-02 12:51:50 +1100

    vue client with initial AJAX

---
+++ 14-advanced/vocab/vocab-client/src/views/Words.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Words</h1>
+    <table id="words" class="ui celled compact table">
+      <thead>
+        <tr>
+          <th>English</th>
+          <th>German</th>
+          <th colspan="3"></th>
+        </tr>
+      </thead>
+
+      <tbody>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { api } from '../helpers/helpers';
+export default {
+  name: 'words',
+  data() {
+    return {
+      words: []
+    }
+  },
+  async mounted() {
+    this.words = await api.getWords();
+  }
+};
+</script>
+
+<style>
+</style>
